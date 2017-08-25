a1ee6e150ee99c869c7704242e97e15f5ae799f4
David Rolfs <davidrolfs15@gmail.com>
2017-08-25 11:30:04 -0700

    api call returns top 3 starred repos

---
+++ src/Portfolio/Models/EnvironmentVariables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class EnvironmentVariables
+    {
+        //8ceccec3df7299da91a8dda82743163140464ce4
+        public static string AccountSid = "8ceccec3df7299da91a8dda82743163140464ce4";
+
+
+          //  https://api.github.com/search/repositories?q=user:davidrolfs&sort=stars&order=desc&page=1&per_page=3?client_id=davidrolfs&client_secret=8ceccec3df7299da91a8dda82743163140464ce4
+    }
+}
